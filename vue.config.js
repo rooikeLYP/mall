b545ae298263f56bd886eee258d886404aa4df4a
rooikeLYP <100846427+rooikeLYP@users.noreply.github.com>
2022-03-05 21:47:34 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+})
